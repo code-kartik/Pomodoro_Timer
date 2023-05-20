ef22a87eeafac8de0b6598cffbc272d9b7afc1a0
code-kartik <kartikarora23082003@gmail.com>
2023-05-20 10:43:17 +0530

    Notifications Added

--- app/src/main/java/com/example/pomodoro/MainActivity.kt
+++ app/src/main/java/com/example/pomodoro/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.pomodoro
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.pomodoro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         supportActionBar?.hide()
-        binding.timerTextView.text = "02:00"
+        binding.timerTextView.text = "25:00"
 
         binding.startButton.setOnClickListener {
             startTimer()
@@ -41,7 +47,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startWorkTimer() {
-        val pomodoroTimeInMillis = 2 * 60 * 1000 // 25 minutes
+        val notifText:String = "You did good work. Take some break☺"
+        val pomodoroTimeInMillis = 25 * 60 * 1000 // 25 minutes
 
         countDownTimer = object : CountDownTimer(pomodoroTimeInMillis.toLong(), 1000) {
             override fun onTick(millisUntilFinished: Long) {
@@ -54,6 +61,7 @@ class MainActivity : AppCompatActivity() {
                 binding.timerTextView.text = "00:00"
                 isWorkTime = false
                 startBreakTimer()
+                showNotification(notifText)
             }
         }
 
@@ -62,7 +70,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startBreakTimer() {
-        val breakTimeInMillis = 1 * 60 * 1000
+        val notifText:String = "Break's Over. Time to get back to work."
+        val breakTimeInMillis = 5 * 60 * 1000
 
         countDownTimer = object : CountDownTimer(breakTimeInMillis.toLong(), 1000) {
             override fun onTick(millisUntilFinished: Long) {
@@ -76,6 +85,7 @@ class MainActivity : AppCompatActivity() {
                 binding.timerTextView.text = "00:00"
                 isWorkTime = true
                 startWorkTimer()
+                showNotification(notifText)
             }
         }
         countDownTimer.start()
@@ -89,5 +99,22 @@ class MainActivity : AppCompatActivity() {
             isWorkTime = false
         }
         binding.timerTextView.text = "02:00"
+    }
+
+    private fun showNotification(context:String) {
+        val builder = NotificationCompat.Builder(this@MainActivity, "pomodoro_channel")
+            .setSmallIcon(R.drawable.baseline_timer_24)
+            .setContentTitle("Pomodoro Timer")
+            .setContentText(context)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+
+        // Display the notification
+        try {
+            val notificationManager = NotificationManagerCompat.from(this@MainActivity)
+            notificationManager.notify(0, builder.build())
+        } catch (e: SecurityException) {
+            Toast.makeText(this, "Notification permission denied. Cannot show notification.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
